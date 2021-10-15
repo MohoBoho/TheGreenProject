84fa46998bd7a356371034313ef7fa69aca6cd0c
metometo <orbit_alkal@yahoo.com>
2021-10-15 13:50:52 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>The Green Project</title>
+  <meta name="description" content="The Green Project">  
+</head>
+
+<body style="background:#DAF7A6; color:tomato;">
+  <h1>The Green Project</h1>
+</body>
+</html>
